720a2dd3c94b0d7d2a5b3e01536fb1c0a8ca886b
maisyhallam <m.hallam@sms.ed.ac.uk>
2024-06-25 15:24:45 +0100

    25th June
    changed edit distance measure to average edit distance rather than mantel

--- code/kintypes.py
+++ code/kintypes.py
@@ -67,3 +67,29 @@ generation_n1 = [
                  'fMyB','fMyZ',
                  'fFeB','fFeZ',
                  'fFyB','fFyZ']
+
+pairs_no_parents = [
+
+# nuncles and cousins
+
+# mother's brother
+
+('mMeB','mMeBS'),('mMeB','mMeBD'),('mMyB','mMyBS'),('mMyB','mMyBD'),
+('fMeB','fMeBS'),('fMeB','fMeBD'),('fMyB','fMyBS'),('fMyB','fMyBD'),
+
+# mother's sister
+    
+('mMeZ','mMeZS'),('mMeZ','mMeZD'),('mMyZ','mMyZS'),('mMyZ','mMyZD'),
+('fMeZ','fMeZS'),('fMeZ','fMeZD'),('fMyZ','fMyZS'),('fMyZ','fMyZD'),
+    
+# father's brother
+    
+('mFeB','mFeBS'),('mFeB','mFeBD'),('mFyB','mFyBS'),('mFyB','mFyBD'),
+('fFeB','fFeBS'),('fFeB','fFeBD'),('fFyB','fFyBS'),('fFyB','fFyBD'),
+    
+# father's sister
+
+('mFeZ','mFeZS'),('mFeZ','mFeZD'),('mFyZ','mFyZS'),('mFyZ','mFyZD'),
+('fFeZ','fFeZS'),('fFeZ','fFeZD'),('fFyZ','fFyZS'),('fFyZ','fFyZD')
+
+]
